9ddf2d5a2ce6a1451b69fff003a03d5261a8408d
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-30 11:53:57 +0530

    Added: Program to add details through console

--- main/java/addressbook/AddressBookMain.java
+++ main/java/addressbook/AddressBookMain.java
@@ -5,19 +5,11 @@ public class AddressBookMain {
         System.out.println("Welcome to AddressBook Program");
 
         AddressBook contact = new AddressBook();
-        contact.setFirstName("Kallavai");
-        contact.setLastName("Sravanthi");
-        contact.setAddress("11/11 abcd colony,efgh nagar");
-        contact.setCity("Hyderabad");
-        contact.setState("ABCD");
-        contact.setZip("344576");
-        contact.setPhoneNumber("7846837489");
-        contact.setEmail("nhydn@yahoo.com");
+        UserInterface userInterface = new UserInterface();
+        userInterface.enterDetails(contact);
 
         ContactStore contactStore = new ContactStore();
         contactStore.add(contact);
-
-        UserInterface userInterface = new UserInterface();
         userInterface.print(contactStore.getContactList());
     }
 }
