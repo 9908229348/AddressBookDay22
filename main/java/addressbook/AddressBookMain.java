0e5f654907988aeb813feb2c7b7282617413427b
9908229348 <kalavaisravanthi98@gmail.com>
2021-10-30 11:40:39 +0530

    Added: Program to add Contacts in AddressBook

--- main/java/addressbook/AddressBookMain.java
+++ main/java/addressbook/AddressBookMain.java
@@ -3,5 +3,21 @@ package addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook Program");
+
+        AddressBook contact = new AddressBook();
+        contact.setFirstName("Kallavai");
+        contact.setLastName("Sravanthi");
+        contact.setAddress("11/11 abcd colony,efgh nagar");
+        contact.setCity("Hyderabad");
+        contact.setState("ABCD");
+        contact.setZip("344576");
+        contact.setPhoneNumber("7846837489");
+        contact.setEmail("nhydn@yahoo.com");
+
+        ContactStore contactStore = new ContactStore();
+        contactStore.add(contact);
+
+        UserInterface userInterface = new UserInterface();
+        userInterface.print(contactStore.getContactList());
     }
 }
